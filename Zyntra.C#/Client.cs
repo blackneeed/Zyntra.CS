4f60f7765817aad007e9318e34e092ecaa405bf7
blackneeed <blackneeed@users.noreply.github.com>
2025-06-15 20:14:05 +0200

    🙏

--- Zyntra.C#/Client.cs
+++ Zyntra.C#/Client.cs
@@ -1,13 +1,18 @@
-﻿using RestSharp;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 using System;
 using System.Buffers.Text;
 using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
+using System.Xml.Xsl;
 
 namespace Zyntra.CS
 {
@@ -23,29 +28,87 @@ namespace Zyntra.CS
 
         private string ConstructBaseEndpoint() => $"api/{api_version}";
         private string ConstructEndpoint(string endpoint) => ConstructBaseEndpoint() + '/' + endpoint;
-        private RestRequest AuthorizeRequest(RestRequest request) => request.AddHeader("Authorization", _token!);
+        private RestRequest AuthorizeRequest(RestRequest request) => request.AddHeader("Authorization", _token!); // calling this with a null token shouldn't happen. initializedproperly should be checked
         private bool InitializedProperly() => _token != null && _client != null && _started;
 
-        private RestRequest ConstructSendMessageToAccessPointRequest(string accessPoint, string content) => AuthorizeRequest(new RestRequest(ConstructEndpoint($"channels/{accessPoint}/messages"), Method.Post).AddJsonBody(new { content }));
-        public async Task<(bool status, string reason)> SendMessageToAccessPoint(string accessPoint, string content)
+        private RestRequest ConstructSendMessageRequest(long accessPoint, string content) => AuthorizeRequest(new RestRequest(ConstructEndpoint($"channels/{accessPoint}/messages"), Method.Post).AddJsonBody(new { content }));
+        public async Task<(bool status, string reason, Message? msg)> SendMessage(long accessPoint, string content)
         {
             if (!InitializedProperly())
             {
-                return (false, "not initialized properly");
+                return (false, "not initialized properly", null);
             }
 
-            RestResponse response = await _client.ExecuteAsync(ConstructSendMessageToAccessPointRequest(accessPoint, content));
+            RestResponse response = await _client!.ExecuteAsync(ConstructSendMessageRequest(accessPoint, content));
             if (response == null)
             {
-                return (false, "response was null");
+                return (false, "response was null", null);
             }
 
             if (response.StatusCode != HttpStatusCode.OK)
             {
-                return (false, "status code was not OK");
+                return (false, "status code was not OK", null);
             }
 
-            return (true, "sent message");
+            if (response.ContentType == null || response.ContentType != "application/json")
+            {
+                return (false, "response content type was not application/json", null);
+            }
+
+            if (response.Content == null)
+            {
+                return (false, "response content was null", null);
+            }
+
+            JObject message_response;
+
+            try
+            {
+                message_response = JObject.Parse(response.Content);
+            }
+            catch
+            {
+                return (false, "response content deserialization failed", null);
+            }
+
+            if (message_response["success"]?.Type != JTokenType.Boolean || !(message_response["success"]?.Value<bool>() ?? false))
+            {
+                return (false, "message creation failed", null);
+            }
+
+            if (message_response["messageId"] == null || message_response["messageId"]?.Type != JTokenType.Integer)
+            {
+                return (false, "unexpected response from server", null);
+            }
+
+            if (message_response["date"] == null || message_response["date"]?.Type != JTokenType.String)
+            {
+                return (false, "unexpected response from server", null);
+            }
+
+            long messageID = message_response["messageId"]!.Value<long>();
+            string date = message_response["date"]!.Value<string>() ?? "";
+
+            DateTime dateParsed = DateTime.MinValue;
+
+            try
+            {
+                dateParsed = DateTime.Parse(date, null, DateTimeStyles.AdjustToUniversal);
+            }
+            catch (FormatException) { }
+
+            Message msg = new Message { DateTime = dateParsed, ID = messageID };
+            return (true, "sent message", msg);
+        }
+
+        private async Task<(bool status, string reason)> DeleteMessage(long accessPoint, long messageId)
+        {
+            return (false, "TBD"); // well szymekk didnt respond ❤️
+        }
+
+        private async Task<(bool status, string reason)> GetMessage(long accessPoint, long messageId)
+        {
+            return (false, "TBD"); // well szymekk didnt respond ❤️
         }
 
         private async Task<(bool status, string reason)> StartBot()
---
+++ Zyntra.C#/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zyntra.CS
+{
+    public class Message
+    {
+        public long ID;
+        public DateTime DateTime;
+    }
+}
